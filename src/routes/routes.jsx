79ef4543974d4a77521b954fa793c0a4f87ac768
bijoymamud <bijoymamud.09@gmail.com>
2025-04-24 00:20:16 +0600

    login page

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,17 +1,28 @@
-import {
-    createBrowserRouter,
-
-} from "react-router-dom";
+import {createBrowserRouter} from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home";
+import Registration from "../Pages/Authentication/Registration";
+import Login from "../Pages/Authentication/Login";
+import DashboardLayout from "../Layout/Admin/DashboardLayout";
   
 
 export const router = createBrowserRouter([
     {
       path: "/",
-        element: <Main />,
+      element: <Main />,
         children: [
           {path: '/', element: <Home/>}
       ]
     },
+
+    //dashboard
+    {
+        path: '/dashboard',
+        element: <DashboardLayout />,
+        children: []
+    },
+
+    //authentication
+    {path: '/registration', element: <Registration/>},
+    {path: '/login', element: <Login/>},
   ]);
