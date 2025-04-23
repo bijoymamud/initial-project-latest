db5e46b730c4e76523c3c8a815096be80322744c
bijoymamud <bijoymamud.09@gmail.com>
2025-04-23 12:01:07 +0600

    project setup with redux

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+          <h1>This is home</h1>
+          <button className="btn btn-soft">Default</button>
+<button className="btn btn-soft btn-primary">Primary</button>
+<button className="btn btn-soft btn-secondary">Secondary</button>
+<button className="btn btn-soft btn-accent">Accent</button>
+<button className="btn btn-soft btn-info">Info</button>
+<button className="btn btn-soft btn-success">Success</button>
+<button className="btn btn-soft btn-warning">Warning</button>
+<button className="btn btn-soft btn-error">Error</button>
+    </div>
+  )
+}
+
+export default Home
